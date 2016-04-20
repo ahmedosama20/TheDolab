662d7adfc4401b806f3cf638e63510baf42a18ca
yomnaa <yomnaismail95@gmail.com>
2016-04-21 01:16:14 +0200

    Update Controller.java

--- app/src/main/java/com/dolab/thedolab/Controller.java
+++ app/src/main/java/com/dolab/thedolab/Controller.java
@@ -17,7 +17,7 @@ public class Controller extends Application {
         
         ClothesFactory Factory= new ClothesFactory();
        clothes= Factory.CreateClothes( id,  type,  note,  colorID);
-       clothes.addToDb();
+       clothes.addToDB();
        
         
         
