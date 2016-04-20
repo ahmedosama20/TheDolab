05176efed5c6b3e8967dc943387e302c1536d259
yomnaa <yomnaismail95@gmail.com>
2016-04-21 00:33:01 +0200

    Update TopTypes.java

--- app/src/main/java/com/dolab/thedolab/TopTypes.java
+++ app/src/main/java/com/dolab/thedolab/TopTypes.java
@@ -4,5 +4,30 @@ package com.dolab.thedolab;
  * Created by OmarAbdulRahman on 4/20/16.
  */
 public enum TopTypes {
-    TSHIRT, SWEATSHIRT, JACKET, SHIRT, BLOUSE, CARDIGAN
+    TSHIRT, SWEATSHIRT, JACKET, SHIRT, BLOUSE, CARDIGAN;
+     public static TopTypes toEnum(int x)
+    {
+        switch(x)
+        {
+            case 0 :
+                return TSHIRT;
+            case 1:
+                return SWEATSHIRT;
+            case 2:
+                return JACKET;
+            case 3:
+                return SHIRT;
+            case 4:
+                return BLOUSE;
+            case 5:
+                return CARDIGAN;
+                            
+                    
+            
+            
+            
+        }
+        
+        
+    }
 }
