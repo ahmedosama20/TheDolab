9797fc425c32c21bd2254348102dd9b78b83fa2f
yomnaa <yomnaismail95@gmail.com>
2016-04-21 00:30:21 +0200

    Update BottomTypes.java

--- app/src/main/java/com/dolab/thedolab/BottomTypes.java
+++ app/src/main/java/com/dolab/thedolab/BottomTypes.java
@@ -4,5 +4,26 @@ package com.dolab.thedolab;
  * Created by OmarAbdulRahman on 4/20/16.
  */
 public enum BottomTypes {
-    SHORT, JEANS, SKIRT, PANTS
+    SHORT, JEANS, SKIRT, PANTS;
+    public static BottomTypes toEnum(int x)
+    {
+        switch(x)
+        {
+            case 0 :
+                return SHORT
+            case 1:
+                return JEANS
+            case 2:
+                return SKIRT
+            case 3 :
+                return PANTS
+                            
+                    
+            
+            
+            
+        }
+        
+        
+    }
 }
