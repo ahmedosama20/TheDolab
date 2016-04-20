b002f051e145034df1924a64ba1743eb7ec896e1
OmarAbdulRahman <o.3ammor@gmail.com>
2016-04-20 22:28:14 +0200

    Added ID - OA

--- app/src/main/java/com/dolab/thedolab/Clothes.java
+++ app/src/main/java/com/dolab/thedolab/Clothes.java
@@ -4,10 +4,12 @@ package com.dolab.thedolab;
  * Created by OmarAbdulRahman on 4/20/16.
  */
 public class Clothes {
+    int ID;
     String Note;
     byte[] Image;
     int ColorID;
     public Clothes(String note, byte[] image, int colorID){
+        ID = 0;
         Note = note;
         Image = image;
         ColorID = colorID;
