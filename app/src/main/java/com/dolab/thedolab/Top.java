c4d893b1846a71b0d619cf7e6a9ff8a427b6f86c
yomnaa <yomnaismail95@gmail.com>
2016-04-21 02:51:20 +0200

    Update Top.java

--- app/src/main/java/com/dolab/thedolab/Top.java
+++ app/src/main/java/com/dolab/thedolab/Top.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  */
 public class Top extends Clothes {
     TopTypes Type;
+    public Top();
     public Top(TopTypes type, String note, int colorID){
         super(note, colorID);
         Type = type;
