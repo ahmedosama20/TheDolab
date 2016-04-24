fc45e0b57390ba8862751ed6ef2a58798c924403
Ahmed Osama <ahmedossama20@gmail.com>
2016-04-24 02:01:50 +0200

    Cleaned the addition UI - AO

--- app/src/main/java/com/dolab/thedolab/AdditionActivity.java
+++ app/src/main/java/com/dolab/thedolab/AdditionActivity.java
@@ -2,6 +2,9 @@ package com.dolab.thedolab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class AdditionActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class AdditionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addition);
+        Spinner spinner = (Spinner)findViewById(R.id.ColorSpinner);
+        ArrayAdapter<CharSequence> aa = ArrayAdapter.createFromResource(this, R.array.colors_array, android.R.layout.simple_spinner_item);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(aa);
+
     }
 }
